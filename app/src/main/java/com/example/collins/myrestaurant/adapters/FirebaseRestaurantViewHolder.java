2293030254fb0c0140345ad911011fa3eb63fe2d
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-06-12 15:27:04 +0300

    added an animation using xml format and the app is running.

--- app/src/main/java/com/example/collins/myrestaurant/adapters/FirebaseRestaurantViewHolder.java
+++ app/src/main/java/com/example/collins/myrestaurant/adapters/FirebaseRestaurantViewHolder.java
@@ -1,5 +1,7 @@
 package com.example.collins.myrestaurant.adapters;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -79,19 +81,27 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder  imple
 
     @Override
     public void onItemSelected() {
-        itemView.animate()
-                .alpha(0.7f)
-                .scaleX(0.9f)
-                .scaleY(0.9f)
-                .setDuration(500);
+//        itemView.animate()
+//                .alpha(0.7f)
+//                .scaleX(0.9f)
+//                .scaleY(0.9f)
+//                .setDuration(500);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_on);
+        set.setTarget(itemView);
+        set.start();
     }
 
     @Override
     public void onItemClear() {
-        itemView.animate()
-                .alpha(1f)
-                .scaleX(1f)
-                .scaleY(1f);
+//        itemView.animate()
+//                .alpha(1f)
+//                .scaleX(1f)
+//                .scaleY(1f);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_off);
+        set.setTarget(itemView);
+        set.start();
     }
 
 }
